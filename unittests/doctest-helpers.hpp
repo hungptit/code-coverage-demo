9477e104faafc121c82b8f08e5d90af4a91c0ea6
Hung Dang <hungptit@gmail.com>
2021-03-28 22:23:31 -0400

    Skip formating if format program is not found.

--- unittests/doctest-helpers.hpp
+++ unittests/doctest-helpers.hpp
@@ -12,34 +12,39 @@
 #include "doctest/doctest.h"
 
 namespace std {
-    bool operator==(const Eigen::Triplet<double> &lhs, const Eigen::Triplet<double> &rhs) {
-        if (lhs.row() != rhs.row()) return false;
-        if (lhs.col() != rhs.col()) return false;
+bool operator==(const Eigen::Triplet<double> &lhs,
+                const Eigen::Triplet<double> &rhs) {
+  if (lhs.row() != rhs.row())
+    return false;
+  if (lhs.col() != rhs.col())
+    return false;
 
-        constexpr double epsilon = 1e-8;
-        if (std::isfinite(lhs.value())) {
-            return lhs.value() == doctest::Approx(rhs.value()).epsilon(epsilon);
-        }
+  constexpr double epsilon = 1e-8;
+  if (std::isfinite(lhs.value())) {
+    return lhs.value() == doctest::Approx(rhs.value()).epsilon(epsilon);
+  }
 
-        // NaN
-        if (std::isnan(lhs.value())) {
-            return std::isnan(rhs.value());
-        }
+  // NaN
+  if (std::isnan(lhs.value())) {
+    return std::isnan(rhs.value());
+  }
 
-        // Inf
-        return lhs.value() == rhs.value();
-    }
+  // Inf
+  return lhs.value() == rhs.value();
+}
 
-    bool operator==(const std::vector<Eigen::Triplet<double>> &lhs, const std::vector<Eigen::Triplet<double>> &rhs) {
-        if (lhs.size() != rhs.size()) {
-            return false;
-        }
+bool operator==(const std::vector<Eigen::Triplet<double>> &lhs,
+                const std::vector<Eigen::Triplet<double>> &rhs) {
+  if (lhs.size() != rhs.size()) {
+    return false;
+  }
 
-        for (uint32_t idx = 0; idx < lhs.size(); ++idx) {
-            if (lhs[idx] == rhs[idx]) continue;
-            return false;
-        }
+  for (uint32_t idx = 0; idx < lhs.size(); ++idx) {
+    if (lhs[idx] == rhs[idx])
+      continue;
+    return false;
+  }
 
-        return true;
-    }
+  return true;
+}
 } // namespace std
